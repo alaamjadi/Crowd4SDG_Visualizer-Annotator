b2a7b10158651a96f24f92b5ca4914315216e6c5
alaamjadi <maa4517@gmail.com>
2020-10-18 01:14:24 +0200

    Removing the old pictures in case of new queston or new CSV file. The clicked buttons will be saved in a dictionary.

---
+++ public/helper.js
@@ -0,0 +1,16 @@
+console.log('Hello');
+var tweet_id_01 = '1260475593705090000'
+var clicked_button_01 = 'Yes'
+var tweet_id_02 = '1260244708216860000'
+var clicked_button_02 = 'Yes'
+
+// defining an object
+var myDataObject = {};
+
+myDataObject[tweet_id_01] = clicked_button_01
+myDataObject[tweet_id_02] = clicked_button_02
+
+typeof myDataObject
+
+
+myDataObject
--- public/main.js
+++ public/main.js
@@ -1,10 +1,12 @@
 
 var options = ""
+var myDataObject = {}
 
 /* Copyright 2012-2013 Daniel Tillin
  * csvToArray v2.1 (Unminifiled for development)
  * http://code.google.com/p/csv-to-array/
  */
+
 String.prototype.csvToArray = function (o) {
     var od = {
         'fSep': ',',
@@ -70,17 +72,30 @@ String.prototype.csvToArray = function (o) {
     return a;
 }
 
-/* let currQuestion = 1;
-let numQuestions = 3;
-let progressBar = document.querySelector('#math-popup-progress-bar');
-for (let i = 0; i < numQuestions; i++){
-  progressBar.innerHTML += '<div class="math-popup-progress"></div>'
+function deletChild(id_element) {
+    var e = document.querySelector("#" + id_element);
+    var child = e.firstElementChild;
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
 }
-document.querySelectorAll('.math-popup-progress').forEach(el => el.style.width = "calc(100% * " + currQuestion + " / " + numQuestions + ")")
 
-document.querySelector('.math-popup-progress').classList.add('math-question-current'); */
+function handleClick(tweet_id, options_tag) {
+    for (let index = 0; index < options.length; index++) {
+        $("#btn"+tweet_id+options[index]).removeClass('active')
+    }
+    $("#btn" + tweet_id + options_tag).toggleClass("active")
+    myDataObject[tweet_id] = options_tag
+}
+
+function submit_all() {
+    console.log(myDataObject)
+}
 
 $('#submit-btn').click(function () {
+    myDataObject = {}
+    deletChild('twitter-images')
     var question = $('#question-text').val()
     options = $('#answer-text').val().split(';')
     if (question.length && options.length != 0) {
@@ -92,10 +107,11 @@ $('#submit-btn').click(function () {
 })
 
 $('#file-upload').change(function () {
+    deletChild('twitter-images')
     let i = $(this).prev('label').clone();
     let file = $('#file-upload')[0].files[0];
     $(this).prev('label').text(file.name);
-    console.log(options)
+    //console.log(options)
     if (file) {
         // new FileReader object
         let reader = new FileReader();
@@ -119,42 +135,16 @@ $('#file-upload').change(function () {
                         url = split[1];
                         tag = split[2];
 
-                        document.getElementById('twitter-images').innerHTML += `<div class="d-flex justify-content-center col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 text-center">
-                        <div class="table-responsive">
-                          <table class="table borderless">
-                            <tr>
-                              <td>
-                                <img alt="001" src="${url}" width="200" height="200" title="${tweet_id}">
-                              </td>
-                            </tr>
-                            <tr>
-                            <td>
-                                Tag: ${tag}
-                            </td>
-                            </tr>
-                            <tr>
-                              <td>
-                              <div id="btn${tweet_id}">
-                              </div>
-                              </td>
-                            </tr>
-                          </table>
-                        </div>
-                      </div>`
+                        myDataObject[tweet_id] = "NotClicked"
+
+                        document.getElementById('twitter-images').innerHTML += `<div class="d-flex justify-content-center col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 text-center"><div class="table-responsive"><table class="table borderless"><tr><td><img alt="001" src="${url}" width="200" height="200" title="${tweet_id}"></td></tr><tr><td>Tag: ${tag}</td></tr><tr><td><div id="btn${tweet_id}"></div></td></tr></table></div></div>`
+
                         for (let index = 0; index < options.length; index++) {
-                            document.getElementById(`btn${tweet_id}`).innerHTML += `<button type="button" id="btn${tweet_id}${options[index]}" onclick="handleClick('${tweet_id}', '${options[index]}')" class="button center mr-2">${options[index]}</button>`
+                            document.getElementById(`btn${tweet_id}`).innerHTML += `<button type="button" id="btn${tweet_id}${options[index]}" onclick="handleClick('${tweet_id}', '${options[index]}')" class="button center mr-2 mb-2">${options[index]}</button>`
                         }
                     }
                 }
             }
-            /* var button = document.createElement("BUTTON");
-            var t = document.createTextNode("Submit answers");
-            button.appendChild(t);
-            button.setAttribute('onclick', 'buttonClick()');
-            document.body.appendChild(button); */
-
-            //console.log(text)
-            // document.querySelector("#file-contents").textContent = text;
         });
 
         // event fired when file reading failed
@@ -166,10 +156,3 @@ $('#file-upload').change(function () {
         reader.readAsText(file);
     }
 });
-
-
-function handleClick(tweet_id, options_tag) {
-    $("#btn"+tweet_id+options_tag).toggleClass("active")
-    console.log("Tweet ID: ", tweet_id, " - Button Tag: ", options_tag)
-    console.log("btn"+tweet_id+options_tag)
-}
